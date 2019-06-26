7eab3e18be153f85a6b3ef8b1828700722a0c743
Maksim S <smirn08m@gmail.com>
2019-06-26 22:09:26 +0300

    self style transfer model

--- bot_model.py
+++ bot_model.py
@@ -1,42 +1,45 @@
 import torch
-# import torch.nn as nn
-# import torch.nn.functional as F
-# import torch.optim as optim
-
-# import torchvision.transforms as transforms
-import torchvision.models as models
-
-from PIL import Image
 from scipy import misc
+
+import torchvision.models as models
 
 from model_backend import *
 
 
 class StyleTransferModel:
     def __init__(self):
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        print(self.device)
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        print(f'bot running on: {self.device}')
 
         self.cnn = models.vgg19(pretrained=True).features.eval()
 
     def transfer_style(self, content_img, style_img):
         content_img = process_image(content_img)
-        print(f'content_img info:{content_img}')
         style_img = process_image(style_img)
-        print(f'style_img info:{style_img}')
+
         input_img = content_img.clone()
-        output = self.run_style_transfer(self.cnn, cnn_normalization_mean,
-                                    cnn_normalization_std, content_img,
-                                    style_img, input_img).cpu()
+
+        output = self.run_style_transfer(
+            self.cnn, cnn_normalization_mean,
+            cnn_normalization_std, content_img,
+            style_img, input_img
+        ).cpu()
+
         return misc.toimage(output[0])
 
-    def run_style_transfer(self, cnn, normalization_mean, normalization_std,
-                        content_img, style_img, input_img, num_steps=500,  # <<<<<<<<<<<
-                        style_weight=100000, content_weight=1):
+    def run_style_transfer(
+        self, cnn, normalization_mean, normalization_std,
+        content_img, style_img, input_img, num_steps=500,
+        style_weight=100000, content_weight=1
+    ):
         """Run the style transfer."""
+
         print('Building the style transfer model..')
-        model, style_losses, content_losses = get_style_model_and_losses(cnn,
-            normalization_mean, normalization_std, style_img, content_img, self.device)
+
+        model, style_losses, content_losses = get_style_model_and_losses(
+            cnn, normalization_mean, normalization_std,
+            style_img, content_img, self.device
+        )
         optimizer = get_input_optimizer(input_img)
 
         print('Optimizing..')
@@ -44,8 +47,11 @@ class StyleTransferModel:
         while run[0] <= num_steps:
 
             def closure():
-                # correct the values
-                # это для того, чтобы значения тензора картинки не выходили за пределы [0;1]
+                """correct the values
+                это для того, чтобы значения тензора картинки не
+                выходили за пределы [0;1]
+                """
+
                 input_img.data.clamp_(0, 1)
 
                 optimizer.zero_grad()
@@ -58,7 +64,7 @@ class StyleTransferModel:
                 for cl in content_losses:
                     content_score += cl.loss
 
-                # взвешивание ощибки
+                # взвешивание ошибки
                 style_score *= style_weight
                 content_score *= content_weight
 
@@ -67,9 +73,8 @@ class StyleTransferModel:
 
                 run[0] += 1
                 if run[0] % 50 == 0:
-                    print("run {}:".format(run))
-                    print('Style Loss : {:4f} Content Loss: {:4f}'.format(
-                        style_score.item(), content_score.item()))
+                    print(f'run {run}:')
+                    print(f'Style Loss : {style_score.item():4f} Content Loss: {content_score.item():4f}')
                     print()
 
                 return style_score + content_score
--- model_backend.py
+++ model_backend.py
@@ -10,7 +10,7 @@ from PIL import Image
 from torchvision import transforms
 
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 imsize = 512
 
 
@@ -22,6 +22,7 @@ def process_image(img_stream):
 
     image = Image.open(img_stream)
     image = loader(image).unsqueeze(0)
+
     return image.to(device, torch.float)
 
 
@@ -32,6 +33,7 @@ class ContentLoss(nn.Module):
         # to dynamically compute the gradient: this is a stated value,
         # not a variable. Otherwise the forward method of the criterion
         # will throw an error.
+
         self.target = target.detach()  # это константа. Убираем ее из дерева вычеслений
         self.loss = F.mse_loss(self.target, self.target)  # to initialize with something
 
@@ -46,11 +48,11 @@ def gram_matrix(input):
     # (h,w)=dimensions of a feature map (N=h*w)
 
     features = input.view(batch_size * h, w * f_map_num)  # resise F_XL into \hat F_XL
-
     G = torch.mm(features, features.t())  # compute the gram product
 
     # we 'normalize' the values of the gram matrix
     # by dividing by the number of element in each feature maps.
+
     return G.div(batch_size * h * w * f_map_num)
 
 
@@ -58,13 +60,16 @@ class StyleLoss(nn.Module):
     def __init__(self, target_feature):
         super(StyleLoss, self).__init__()
         self.target = gram_matrix(target_feature).detach()
-        self.loss = F.mse_loss(self.target, self.target) # to initialize with something
+        self.loss = F.mse_loss(self.target, self.target)  # to initialize with something
 
     def forward(self, input):
         G = gram_matrix(input)
         self.loss = F.mse_loss(G, self.target)
         return input
 
+
+# Определим после каких уровней мы будем счиатать ошибки стиля,
+# а после каких ошибки контента
 
 cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406]).to(device)
 cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225]).to(device)
@@ -76,11 +81,12 @@ class Normalization(nn.Module):
         # .view the mean and std to make them [C x 1 x 1] so that they can
         # directly work with image Tensor of shape [B x C x H x W].
         # B is batch size. C is number of channels. H is height and W is width.
+
         self.mean = torch.tensor(mean).view(-1, 1, 1).to(device)
         self.std = torch.tensor(std).view(-1, 1, 1).to(device)
 
     def forward(self, img):
-        # normalize img
+        """normalize img"""
         return (img - self.mean) / self.std
 
 
@@ -88,43 +94,48 @@ content_layers_default = ['conv_4']
 style_layers_default = ['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']
 
 
-def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
-                                   style_img, content_img, device,
-                                   content_layers=content_layers_default,
-                                   style_layers=style_layers_default):
+def get_style_model_and_losses(
+    cnn, normalization_mean, normalization_std, style_img, content_img, device,
+    content_layers=content_layers_default, style_layers=style_layers_default
+):
     print('Model start to build')
+
     cnn = copy.deepcopy(cnn).to(device)
 
     # normalization module
-    normalization = Normalization(normalization_mean, normalization_std).to(device)
+    normalization = Normalization(
+        normalization_mean, normalization_std
+    ).to(device)
 
-    # just in order to have an iterable access to or list of content/syle
-    # losses
+    # just in order to have an iterable access to or
+    # list of content/syle losses
+
     content_losses = []
     style_losses = []
 
     # assuming that cnn is a nn.Sequential, so we make a new nn.Sequential
     # to put in modules that are supposed to be activated sequentially
+
     model = nn.Sequential(normalization)
 
     i = 0  # increment every time we see a conv
     for layer in cnn.children():
         if isinstance(layer, nn.Conv2d):
             i += 1
-            name = 'conv_{}'.format(i)
+            name = f'conv_{i}'
         elif isinstance(layer, nn.ReLU):
-            name = 'relu_{}'.format(i)
-            # The in-place version doesn't play very nicely with the ContentLoss
-            # and StyleLoss we insert below. So we replace with out-of-place
-            # ones here.
-            # Переопределим relu уровень
-            layer = nn.ReLU(inplace=False)
+            name = f'relu_{i}'
+            # The in-place version doesn't play very nicely with the
+            # ContentLoss and StyleLoss we insert below.
+            # So we replace with out-of-place ones here.
+
+            layer = nn.ReLU(inplace=False)  # Переопределим relu уровень.
         elif isinstance(layer, nn.MaxPool2d):
-            name = 'pool_{}'.format(i)
+            name = f'pool_{i}'
         elif isinstance(layer, nn.BatchNorm2d):
-            name = 'bn_{}'.format(i)
+            name = f'bn_{i}'
         else:
-            raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+            raise RuntimeError(f'Unowned layer: {layer.__class__.__name__}')
 
         model.add_module(name, layer)
 
@@ -132,29 +143,34 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
             # add content loss:
             target = model(content_img).detach()
             content_loss = ContentLoss(target)
-            model.add_module("content_loss_{}".format(i), content_loss)
+            model.add_module(f'content_loss_{i}', content_loss)
             content_losses.append(content_loss)
 
         if name in style_layers:
             # add style loss:
             target_feature = model(style_img).detach()
             style_loss = StyleLoss(target_feature)
-            model.add_module("style_loss_{}".format(i), style_loss)
+            model.add_module(f'style_loss_{i}', style_loss)
             style_losses.append(style_loss)
 
     # now we trim off the layers after the last content and style losses
     # выбрасываем все уровни после последенего styel loss или content loss
+
     for i in range(len(model) - 1, -1, -1):
         if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
             break
 
     model = model[:(i + 1)]
     print('Model end to build')
+
     return model, style_losses, content_losses
 
 
 def get_input_optimizer(input_img):
-    # this line to show that input is a parameter that requires a gradient
-    # добоваляет содержимое тензора катринки в список изменяемых оптимизатором параметров
+    """his line to show that input is a parameter that requires a gradient
+    добоваляет содержимое тензора катринки в список
+    изменяемых оптимизатором параметров
+
+    """
     optimizer = optim.LBFGS([input_img.requires_grad_()])
     return optimizer
