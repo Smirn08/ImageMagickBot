a4c45bf2053de3541d641289e46505aa68c5bbe3
Maksim S <smirn08m@gmail.com>
2019-06-26 22:11:30 +0300

    main bot file

--- main_bot.py
+++ main_bot.py
@@ -1,10 +1,13 @@
+import secrets
 import logging
+import threading
 from io import BytesIO
 
 import settings
 import fast_neural_style
-from bot_model import StyleTransferModel
 from my_token_proxy import TOKEN, PROXY
+from bot_model import StyleTransferModel
+
 
 from telegram import InlineKeyboardMarkup
 
@@ -24,7 +27,8 @@ first_image_file = {}
 
 
 def antispam(bot, update):
-    # удаляет вест флуд пользователя
+    """Удаляет вест флуд пользователя."""
+
     chat_id = update.message.chat_id
     bot.delete_message(
         chat_id=chat_id, message_id=update.message.message_id
@@ -32,10 +36,27 @@ def antispam(bot, update):
     print(update.message.text)
 
 
+def information(bot, update):
+    """Страница ИНФО."""
+
+    query = update.callback_query
+    bot.edit_message_text(
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id,
+        text=settings.ABOUT,
+        reply_markup=InlineKeyboardMarkup(settings.INFO_KEYS),
+        parse_mode='Markdown',
+    )
+
+
 def start(bot, update):
+    """Приветствие пользователя."""
+
+    settings.STATUS = None
+    settings.MODE = None
     text = f'''Хм, здорова *{update.message.chat.first_name}*, покреативим?
-    Я тут с картинками балуюсь, присоединяйся :>
-    Выбирай, что хочешь сделать?'''
+Я тут с картинками балуюсь, присоединяйся :>
+Выбирай, что хочешь сделать?'''
     chat_id = update.message.chat_id
     bot.send_message(
         chat_id=chat_id,
@@ -47,6 +68,11 @@ def start(bot, update):
 
 
 def menu_operation(bot, update):
+    """Главное меню с выбором стилей."""
+
+    settings.STATUS = None
+    settings.MODE = None
+    print(f'user style: {settings.MODE}')
     query = update.callback_query
     bot.edit_message_text(
         chat_id=query.message.chat_id,
@@ -58,79 +84,99 @@ def menu_operation(bot, update):
 
 
 def handle_operation(bot, update):
+    """Обработчик выбора стиля."""
+
     query = update.callback_query
     chat_id = query.message.chat_id
-    mode = update.callback_query.data
+    mode = query.data
     if mode == "self":
-        bot.send_message(chat_id=chat_id,
-                         text=f'''Грузи две картинки!
-Первая - к которой хочешь применить стиль, вторая - сам стиль''')
+        bot.edit_message_text(
+            chat_id=chat_id,
+            message_id=query.message.message_id,
+            text=f'''{secrets.choice(settings.SELF_PHRASE)}''',
+            reply_markup=InlineKeyboardMarkup(settings.CHANGE_KEY)
+        )
         settings.MODE = 'self'
-        print(f'{settings.MODE}')
+        print(f'user style: {settings.MODE}')
     if mode == "candy":
-        bot.send_message(chat_id=chat_id,
-                         text=f'''Хороший выбор. Грузи картинку. Попробуем...''')
+        bot.edit_message_text(
+            chat_id=chat_id,
+            message_id=query.message.message_id,
+            text=f'''{secrets.choice(settings.CHOICE_PHRASES)}''',
+            reply_markup=InlineKeyboardMarkup(settings.CHANGE_KEY)
+        )
         settings.MODE = 'candy'
-        print(f'{settings.MODE}')
+        print(f'user style: {settings.MODE}')
     if mode == "mosaic":
-        bot.send_message(chat_id=chat_id,
-                         text=f'''Это будет легко. Давай сюда картинку...''')
+        bot.edit_message_text(
+            chat_id=chat_id,
+            message_id=query.message.message_id,
+            text=f'''{secrets.choice(settings.CHOICE_PHRASES)}''',
+            reply_markup=InlineKeyboardMarkup(settings.CHANGE_KEY)
+        )
         settings.MODE = 'mosaic'
-        print(f'{settings.MODE}')
+        print(f'user style: {settings.MODE}')
     if mode == "udnie":
-        bot.send_message(chat_id=chat_id,
-                         text=f'''Какое то странное название, вроде бы известная картина. Но да ладно, грузи картинку...''')
+        bot.edit_message_text(
+            chat_id=chat_id,
+            message_id=query.message.message_id,
+            text=f'''{secrets.choice(settings.CHOICE_PHRASES)}''',
+            reply_markup=InlineKeyboardMarkup(settings.CHANGE_KEY)
+        )
         settings.MODE = 'udnie'
-        print(f'{settings.MODE}')
+        print(f'user style: {settings.MODE}')
     if mode == "princess":
-        bot.send_message(chat_id=chat_id,
-                         text=f'''Мое любимое. Давай картинку, покажу как у меня получается творить!''')
+        bot.edit_message_text(
+            chat_id=chat_id,
+            message_id=query.message.message_id,
+            text=f'''{secrets.choice(settings.CHOICE_PHRASES)}''',
+            reply_markup=InlineKeyboardMarkup(settings.CHANGE_KEY)
+        )
         settings.MODE = 'princess'
-        print(f'{settings.MODE}')
+        print(f'user style: {settings.MODE}')
 
 
 def style_operation(bot, update):
-    """Выбор действия с картинкой"""
-
-    settings.STATUS = None
+    """Выбор действия с картинкой."""
 
     chat_id = update.message.chat_id
-    print("Got image from {}".format(chat_id))
+    fn = update.message.from_user['first_name']
+    un = update.message.from_user['username']
+
+    print(f'Got image from {fn} | @{un} ({chat_id})')
 
     # получаем информацию о картинке
     image_info = update.message.photo[-1]
     image_file = bot.get_file(image_info)
-    print(f'pic info:{image_file}')
+    print(f'Image info: {image_file}')
 
     if settings.MODE is None:
         bot.send_message(
                 chat_id=chat_id,
                 message_id=update.message.message_id,
-                text=f'''Стоп, стоп, стоп! Так не пойдет! А что делать то? Не выбрал!''',
+                text=f'''{secrets.choice(settings.STOP_PHRASES)}''',
                 reply_markup=InlineKeyboardMarkup(settings.START_KEY),
                 parse_mode='Markdown'
         )
     elif settings.MODE == 'self':
-        print(f'Проверка_0: {first_image_file}')
         if chat_id in first_image_file:
             # первая картинка, которая к нам пришла станет content image, а вторая style image
             content_image_stream = BytesIO()
-            print(f'main pic bytes info:{content_image_stream}')
             first_image_file[chat_id].download(out=content_image_stream)
             del first_image_file[chat_id]
-            print(f'Проверка_2: {first_image_file}')
+
             style_image_stream = BytesIO()
-            print(f'style pic bytes info:{content_image_stream}')
             image_file.download(out=style_image_stream)
-            print(f'Проверка_3: {first_image_file}')
             bot.send_message(
                 chat_id=chat_id,
                 message_id=update.message.message_id,
-                text=f'''Ну теперь жди ~ 2.5 минуты результат. Процесс не быстрый, сам понимаешь :>''',
+                text=f'''{secrets.choice(settings.WAIT_PHRASES)}''',
                 parse_mode='Markdown'
             )
 
-            output = model.transfer_style(content_image_stream, style_image_stream)
+            output = model.transfer_style(
+                content_image_stream, style_image_stream
+            )
 
             # теперь отправим назад фото
             output_stream = BytesIO()
@@ -138,16 +184,14 @@ def style_operation(bot, update):
             output_stream.seek(0)
             bot.send_photo(chat_id, photo=output_stream)
 
-            print("Sent Photo to user")
+            print(f'Sent style image to {fn} | @{un} ({chat_id})')
             settings.MODE = None
             settings.STATUS = 'DONE'
         else:
             first_image_file[chat_id] = image_file
-            print(f'Проверка_1: {first_image_file}')
     else:
         first_image_file[chat_id] = image_file
         content_image_stream = BytesIO()
-        print(f'main pic bytes info:{content_image_stream}')
         first_image_file[chat_id].download(out=content_image_stream)
         del first_image_file[chat_id]
         if settings.MODE == 'candy':
@@ -159,31 +203,38 @@ def style_operation(bot, update):
         if settings.MODE == 'princess':
             output = fast_neural_style.stylize(content_image_stream, 'udnie.pth')
 
-        print(f'{output}')
-
         output_stream = BytesIO()
         output.save(output_stream, format='PNG')
         output_stream.seek(0)
         bot.send_photo(chat_id, photo=output_stream)
 
-        print("Sent Photo to user")
-        print('>UDNIE<')
-
+        print(f'Sent style image to {fn} | @{un} ({chat_id})')
         settings.MODE = None
         settings.STATUS = 'DONE'
-    print(f'SS:{settings.STATUS}')
+
     if settings.STATUS == 'DONE':
         bot.send_message(
                 chat_id=chat_id,
                 message_id=update.message.message_id,
-                text=f'''По мне так прикольно получилось. Еще попробуем?''',
+                text=f'''{secrets.choice(settings.END_PHRASES)}''',
                 reply_markup=InlineKeyboardMarkup(settings.ACTION_KEYS),
                 parse_mode='Markdown'
         )
+        settings.STATUS = None
+
+
+def shutdown():
+    """Завершение работы."""
+    mybot.stop()
+    mybot.is_idle = False
+
+
+def stop(bot, update):
+    """Завершение работы."""
+    threading.Thread(target=shutdown).start()
 
 
 def main():
-    mybot = Updater(TOKEN, request_kwargs=PROXY)
 
     logging.info('Бот запускается')
 
@@ -192,11 +243,16 @@ def main():
     dp.add_handler(MessageHandler(Filters.text, antispam))
     dp.add_handler(MessageHandler(Filters.photo, style_operation))
     dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('forcestoponpc', stop))  # secret :>
     dp.add_handler(CallbackQueryHandler(menu_operation, pattern='menu'))
+    dp.add_handler(CallbackQueryHandler(information, pattern='info'))
     dp.add_handler(CallbackQueryHandler(handle_operation))
 
     mybot.start_polling()
     mybot.idle()
+
+
+mybot = Updater(TOKEN, request_kwargs=PROXY)
 
 
 if __name__ == '__main__':
